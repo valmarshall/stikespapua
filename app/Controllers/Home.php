c74f40ee32d1156340de4c5ed43ecbef44712ffd
Muhammad Rivaldi <rivaldimuhammad4897@gmail.com>
2021-04-26 09:22:51 +0900

    menambahkan title

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,10 @@ class Home extends BaseController
 {
 	public function index()
 	{
-		return view('index.php');
+		$data = [
+			'title' => 'STIKES Papua ~ Home'
+		];
+
+		return view('index.php', $data);
 	}
 }
